79ea1daf9503038b39bd98b2d2600fe42947e5e2
marianaknust <77455775+marianaknust@users.noreply.github.com>
2021-05-11 13:27:06 +0200

    Remove Console.log

--- src/app.js
+++ src/app.js
@@ -77,8 +77,6 @@ function displayForecast(response){
     forecast.forEach(function (forecastDay,index) {
       
       let date = new Date(forecastDay.dt * 1000);
-
-      console.log(date)
       
       if (index<6) {
 
@@ -102,17 +100,9 @@ function displayForecast(response){
   }
 
   function getForecast(coordinates) {
-  console.log(coordinates);
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
-
-
-
-    
-  
-
 
 function displayCity(city) {
     let cityElement = document.querySelector("#current-city");
